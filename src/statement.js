82a7586083132e91bae417e11f5ea98dc2a4ee59
Jacob <ajdkfl6445@gmail.com>
2020-08-29 01:34:47 +0900

    refactor:  amoutFor 함수의 변수명 변경 perf를 aPerformance로 변경

--- src/statement.js
+++ src/statement.js
@@ -1,21 +1,21 @@
 // 함수 추출하기
-function amountFor(perf, play) {
+function amountFor(aPerformance, play) {
   // 값이 바뀌지 않는 변수는 매개변수로 전달
   let result = 0;
 
   switch (play.type) {
     case "tragedy":
       result = 40000;
-      if (perf.audience > 30) {
-        result += 1000 * (perf.audience - 30);
+      if (aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30);
       }
       break;
     case "comedy":
       result = 30000;
-      if (perf.audience > 20) {
-        result += 10000 + 500 * (perf.audience - 20);
+      if (aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance.audience - 20);
       }
-      result += 300 * perf.audience;
+      result += 300 * aPerformance.audience;
       break;
     default:
       throw new Error(`알 수 없는 장르: ${play.type}`);
