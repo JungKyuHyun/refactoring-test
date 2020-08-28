2ab04b3c1a488b65309d8ad4ef04a259d018a090
Jacob <ajdkfl6445@gmail.com>
2020-08-29 01:09:30 +0900

    init: 리펙토링 전 코드 작성

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import "./styles.css";
 import { statement } from "./statement";
-import * as invoices from "./data/invoices.json";
-import * as plays from "./data/plays.json";
+import invoices from "./data/invoices.json";
+import plays from "./data/plays.json";
 
 export default function App() {
   return (
     <div className="App">
-      {statement(invoices, plays)}
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Jacob's Playground</h1>
+      <p style={{ whiteSpace: "pre-line", lineHeight: "2" }}>
+        {statement(invoices[0], plays)}
+      </p>
     </div>
   );
 }
