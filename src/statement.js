fb607214f4db08007a1026e05dc422e33779acf8
Jacob <jacob@class101.net>
2020-08-30 02:34:44 +0900

    refactor: 각 함수를 맞게 파일로 분리

--- src/statement.js
+++ src/statement.js
@@ -1,73 +1,7 @@
+import createStatementData from "./createStatementData";
+
 function statement(invoice, plays) {
-  const statementData = {};
-  statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances.map(enrichPerformance);
-  statementData.tatalVolumeCredits = tatalVolumeCredits(statementData);
-  statementData.totalAmout = totalAmout(statementData);
-  return renderPlainText(statementData, plays);
-
-  function enrichPerformance(aPerformance) {
-    const result = Object.assign({}, aPerformance);
-    result.play = playFor(result);
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
-    return result;
-  }
-
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-  }
-
-  function amountFor(aPerformance) {
-    let result = 0;
-
-    switch (aPerformance.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-    }
-    return result;
-  }
-
-  function volumeCreditsFor(perf) {
-    let result = 0;
-    result += Math.max(perf.audience - 30, 30);
-    if ("comedy" === perf.play.type) {
-      result += Math.floor(perf.audience / 5);
-    }
-    return result;
-  }
-
-  function totalAmout(data) {
-    return data.performances.reduce((total, p) => (total += p.amount), 0);
-  }
-
-  function tatalVolumeCredits(data) {
-    return data.performances.reduce(
-      (total, p) => (total += p.volumeCredits),
-      0
-    );
-  }
-}
-
-function usd(aNumber) {
-  return new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  }).format(aNumber / 100);
+  return renderPlainText(createStatementData(invoice, plays));
 }
 
 function renderPlainText(data) {
@@ -79,6 +13,14 @@ function renderPlainText(data) {
   result += ` 총액: ${data.totalAmout}\n`;
   result += ` 적립 포인트: ${data.tatalVolumeCredits}점\n`;
   return result;
+}
+
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(aNumber / 100);
 }
 
 export { statement };
