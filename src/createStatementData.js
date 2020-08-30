25f68fd22fa48f5b480472463a3081582b20ed92
Jacob <jacob@class101.net>
2020-08-30 17:44:45 +0900

    refactor: 상속한 서브클래스를 사용하도록 로직을 변경한다

--- src/createStatementData.js
+++ src/createStatementData.js
@@ -36,8 +36,16 @@ export default function createStatementData(invoice, plays) {
 }
 
 // 자바스크립트에서는 생성자가 서브클래스의 인스턴스를 반환할 수 없기 때문에, 생성자를 팩터리 함수로 바꾼다
+// 함수를 이용하면 다음과 같이 PerformanceCalculator의 서브 클래스 중에서 어느 것을 생성해서 반환할지 선택할 수 있다.
 function createPerformanceCalculator(aPerformance, aPlay) {
-  return new PerformanceCalculator(aPerformance, aPlay);
+  switch (aPlay.type) {
+    case "tragedy":
+      return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy":
+      return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`알 수 없는 장르: ${aPlay.type}`);
+  }
 }
 
 // 모든 데이터 변환을 한 곳에서 수행할 수 있어서 코드가 더욱 명확해진다.
@@ -48,25 +56,7 @@ class PerformanceCalculator {
   }
 
   get amount() {
-    let result = 0;
-    switch (this.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (this.performance.audience > 30) {
-          result += 1000 * (this.performance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (this.performance.audience > 20) {
-          result += 10000 + 500 * (this.performance.audience - 20);
-        }
-        result += 300 * this.performance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${this.performance.play.type}`);
-    }
-    return result;
+    throw new Error("서브클래스에서 처리하도록 설계되었습니다");
   }
 
   get volumeCredits() {
@@ -75,6 +65,27 @@ class PerformanceCalculator {
     if ("comedy" === this.play.type) {
       result += Math.floor(this.performance.audience / 5);
     }
+    return result;
+  }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 40000;
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30);
+    }
+    return result;
+  }
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 30000;
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20);
+    }
+    result += 300 * this.performance.audience;
     return result;
   }
 }
